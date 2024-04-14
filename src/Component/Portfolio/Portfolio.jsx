487e4b49e502c994e7b3c429de760a7110c79b07
Praveen-Raghav97gir config --global user.email rishavthakur9027@gmail.com <158564215+Praveen-Raghav97@users.noreply.github.com>
2024-04-14 13:18:54 +0530

    first-commit

--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -5,6 +5,7 @@ import next_icon from '../../assets/next-icon.png'
 import e_icon from '../../assets/8344.jpg'
 import E_commerce from '../../assets/E-Commerce.png'
 import Collage_web from '../../assets/Collage-Website.png'
+import Food_web from '../../assets/Food-Website.png'
 import { useRef } from 'react'
 const Portfolio = () => {
 
@@ -65,15 +66,18 @@ Creating a collage web application using React.js involves structuring component
           </li>
           <li>
             <div className="slide">
-              <img src={e_icon} alt="" />
+              <img src={Food_web} alt="" />
               <div className='Project-info'>
-                <h3>Amozon clone</h3>
-                <span>--E-coomerce--</span>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, suscipit.</p>
+                <h3>Food-Website</h3>
+                <span>---Food---</span>
+                <p>
+A food website built using React JS offers a dynamic and immersive experience for culinary enthusiasts. Leveraging React's component-based architecture, developers can create a seamless user interface with features like interactive recipe displays, search functionality</p>
                 </div>
               <div className="button">
-                <button className='btn dark-btn'> Live Preview</button>
-                <button className=' btn dark-btn'> Source Code</button>
+               
+                <a href="https://food-website-one-bay.vercel.app/"><button className='btn dark-btn'> Live Preview</button> </a>
+                <a href="https://github.com/Praveen-Raghav97/Food-Website"><button className='btn dark-btn'> Source Code</button> </a>
+               
               </div>
             </div>
           </li>
