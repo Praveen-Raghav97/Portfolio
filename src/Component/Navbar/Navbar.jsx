d27a2066407db21d861a7dd7a0cda4b3e34ab5d8
Praveen-Raghav97gir config --global user.email rishavthakur9027@gmail.com <158564215+Praveen-Raghav97@users.noreply.github.com>
2024-05-17 11:14:49 +0530

    scroll

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -20,8 +20,8 @@ const Navbar = () => {
     <nav className={`container  ${sticky?  'dark-nav' : ''}`}>
     <h1>Pra <span>veen.</span></h1>
     <ul className={mobileMenu?'':'hide-mobile-menu'}>
-      <li><Link to='home' smooth ={true} offset ={-70} duration={800} >Home</Link></li>
-      <li><Link to='services' smooth ={true} offset ={-270} duration={500} >Services</Link></li>
+      <li><Link to='home' smooth ={true} offset ={0} duration={700} >Home</Link></li>
+      <li><Link to='services' smooth ={true} offset ={-260} duration={500} >Services</Link></li>
       <li><Link to='about' smooth ={true} offset ={-290} duration={800} >About </Link></li>
       <li><Link to='skills' smooth ={true} offset ={-170} duration={500}  >Skills </Link></li>
       <li><Link to='portfolio' smooth ={true} offset ={-180} duration={500} >Portfolio</Link></li>
